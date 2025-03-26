27a2268be0eb61a3dffe3fdfdabc8f93fcbf4fa3
hari1614 <hariharanofficial2812@gmail.com>
2025-03-26 15:02:39 +0530

    Updated.

---
+++ src/app/leavemanagement/leaveapproval/LeaveApproval.tsx
@@ -0,0 +1,8 @@
+export default function LeaveApproval() {
+    return (
+      <div className="py-14">
+        <h1 className="text-[22px] font-normal leading-[32px]">Leave Approval</h1>
+        
+      </div>
+    );
+  }
---
+++ src/app/leavemanagement/leavepolicy/LeavePolicy.tsx
@@ -0,0 +1,7 @@
+export default function LeavePolicy() {
+    return (
+      <div className="py-14">
+        <h1 className="text-[22px] font-normal leading-[32px]">Leave Policy</h1>
+      </div>
+    );
+  }
