b9b4e5b6920d90e71e38bf94c3ba8c3d2d72b0e7
Sriram281 <sriramc2810@gmail.com>
2025-04-03 16:25:04 +0530

    updated.

--- src/app/internship/addInternFields/rowThree.tsx
+++ src/app/internship/addInternFields/rowThree.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
-import { MdOutlineCancel } from "react-icons/md";
-
 import FormField from "@/app/utils/formField";
 import SelectDropdown from "@/app/utils/selectDropdown";
 
@@ -10,22 +8,6 @@ interface FormProps {
   register: ReturnType<typeof useForm>["register"] | any;
   errors: Record<string, any>;
 }
-
-const inputFields = [
-  { name: "firstName", label: "First Name" },
-  { name: "lastName", label: "Last Name" },
-  { name: "dateOfBirth", label: "Date of Birth" },
-  { name: "email", label: "Email" },
-  { name: "contact", label: "Contact" },
-  { name: "intId", label: "INT ID" },
-  { name: "gender", label: "Gender" },
-  { name: "role", label: "Role" },
-  { name: "courseContent", label: "Course Content" },
-  { name: "dateOfJoining", label: "Date of Joining" },
-  { name: "durationStart", label: "Duration Start" },
-  { name: "durationEnd", label: "Duration End" },
-  { name: "mentor", label: "Mentor" },
-];
 
 const RowThree: React.FC<FormProps> = ({ register, errors }) => {
   return (
